60331eaf4e6bdb098a3a8adeb314ca6d8f0b4600
krishnann-s <krishnan.spu@gmail.com>
2025-03-15 21:10:09 +0530

    feat: add products, productCart and ProductViewModal components and customize color

--- EshoppingZone-React/src/App.tsx
+++ EshoppingZone-React/src/App.tsx
@@ -1,8 +1,10 @@
+import Products from "./components/Products";
+
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen text-2xl">
-      <h1 className="text-banner-color4 font-medium"> Vite Typescript</h1>
-    </div>
+    <>
+      <Products />
+    </>
   );
 }
 
