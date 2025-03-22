0149129411e3f0b9b9b200635e1df267fa6ed8a3
krishnann-s <krishnan.spu@gmail.com>
2025-03-22 12:31:51 +0530

    feat: add openapi documentation in profile service application

--- EShoppingZone/Profile-Service/src/main/java/com/eshopingzone/profileservice/ProfileServiceApplication.java
+++ EShoppingZone/Profile-Service/src/main/java/com/eshopingzone/profileservice/ProfileServiceApplication.java
@@ -4,8 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
 @ComponentScan(basePackages = "com.eshopingzone.profileservice")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "User Profile Microservice Rest API Documentation",
+				description = "Eshoppingzone User Profile Microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Krishnan S",
+						email = "krishnan.spu@gmail.com"
+						),
+				license = @License(
+						name = "Apache 2.0"
+						)
+				)
+)
 public class ProfileServiceApplication {
 
 	public static void main(String[] args) {
